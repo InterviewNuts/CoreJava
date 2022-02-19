fa807c05113a43f26019ba9a5c78aee25321301c
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-02-19 19:03:39 +0530

    Static methods

--- CoreJavaPractice/src/StaticBlockOrder/StaticBlockOrder.java
+++ CoreJavaPractice/src/StaticBlockOrder/StaticBlockOrder.java
@@ -1,8 +1,17 @@
 package StaticBlockOrder;
 
 
+/*
+static variable  is declared outside any method and it goes method area & loaded when the class is loaded
+ */
 public class StaticBlockOrder {
 	static StaticBlockOrder objMember = new StaticBlockOrder(20);
+	
+	void test_local_static()
+	{
+		 String s= "hara";
+		
+	}
 	static {
 		System.out.println("Called from static Block");
 		StaticBlockOrder objMember = new StaticBlockOrder(25);
